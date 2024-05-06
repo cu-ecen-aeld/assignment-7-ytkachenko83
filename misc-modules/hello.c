3a84fcb094e366639b7153906540a263e909e485
Yuri Tkachenko <yura.tkachenko@gmail.com>
2024-05-06 00:45:55 -0700

    minor change to the hello drv

--- misc-modules/hello.c
+++ misc-modules/hello.c
@@ -5,15 +5,17 @@
 #include <linux/module.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
+#define GITHUB_USER "ytkachenko83"
+
 static int hello_init(void)
 {
-	printk(KERN_ALERT "Hello, world - %s\n", "ytkachenko83");
+	printk(KERN_ALERT "Hello, world // %s\n", GITHUB_USER);
 	return 0;
 }
 
 static void hello_exit(void)
 {
-	printk(KERN_ALERT "Goodbye, cruel world\n");
+	printk(KERN_ALERT "Goodbye, cruel world // %s\n", GITHUB_USER);
 }
 
 module_init(hello_init);
